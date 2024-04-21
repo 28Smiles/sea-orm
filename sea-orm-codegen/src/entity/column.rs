0860eee68c68326178f53c5d5dc237b2dc0b0e50
Leon Camus <leon.c@gmx.de>
2024-04-21 13:03:37 +0200

    add column type

--- sea-orm-codegen/src/entity/column.rs
+++ sea-orm-codegen/src/entity/column.rs
@@ -75,6 +75,7 @@ impl Column {
                 ColumnType::Array(column_type) => {
                     format!("Vec<{}>", write_rs_type(column_type, date_time_crate))
                 }
+                ColumnType::Vector => "::pgvector::Vector".to_owned(),
                 _ => unimplemented!(),
             }
         }
